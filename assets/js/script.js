68111e2e2ecaa8ba2ac8181685ca99874b444861
Sourav Mondal <108756502+Sourav-gittech@users.noreply.github.com>
2025-03-06 20:21:55 +0530

    commited

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// show / hide navbar on scroll 
 let navbar = document.querySelector('.navbar');
 
 window.onscroll = ()=>{
@@ -8,3 +9,25 @@ window.onscroll = ()=>{
         navbar.classList.remove('shadow-sm');
     }
 }
+
+
+// counter design 
+document.addEventListener('DOMContentLoaded', () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter('count1', 0, 100, 3000);
+    counter('count2', 0, 90, 3000);
+    counter('count3', 0, 95, 3000);
+});
